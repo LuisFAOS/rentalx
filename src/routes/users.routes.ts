53c8c34e0ca619b9ba24e97db1fc81c8cdc66999
Luis FAOS <luisfaos.web@gmail.com>
2021-12-02 13:27:39 -0300

    splitting responsibilities (multer and api routes)

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,21 @@
 import { Router } from 'express'
+import multer from "multer"
+import { uploadConfig } from '../config/upload'
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated'
+
 import { CreateUserController } from '../modules/accounts/useCases/createUser/CreateUserController'
+import { UpdateUserAvatarController } from '../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController'
+
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"))
 
 const usersRoutes = Router()
 
 usersRoutes.post("/", new CreateUserController().handle)
+
+usersRoutes.patch("/avatar", 
+   ensureAuthenticated,
+   uploadAvatar.single("file"),
+   new UpdateUserAvatarController().handle
+)
 
 export { usersRoutes }
