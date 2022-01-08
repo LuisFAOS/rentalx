264b2a9ba77965c9eb3003db28b06f72a42cbe02
LuisFAOS <luisfaos.web@gmail.com>
2022-01-07 22:38:11 -0300

    controller interface

---
+++ src/usualInterfaces/IControllers.ts
@@ -0,0 +1,16 @@
+import { Request } from "express";
+
+let req: Request
+
+type othersType = {
+   params?: typeof req.params,
+   user_id?: string,
+   file?: typeof req.file,
+   files?: typeof req.files 
+}
+type queryParamsType = typeof req.query
+
+export interface IControllers {
+   handle(queryParams?: queryParamsType, body?: any, others?: othersType): 
+      Promise<{status: "created" | "ok", result: Object}>
+}
