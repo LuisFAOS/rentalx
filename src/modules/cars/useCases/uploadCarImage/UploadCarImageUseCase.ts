3ae04c4295d59a071d191c9493d12832f0bd3e26
LuisFAOS <luisfaos.web@gmail.com>
2022-02-05 20:10:33 -0300

    added storage provider

--- src/modules/cars/useCases/uploadCarImage/UploadCarImageUseCase.ts
+++ src/modules/cars/useCases/uploadCarImage/UploadCarImageUseCase.ts
@@ -1,4 +1,6 @@
 import { inject, injectable } from "tsyringe";
+import injectionTokensMap from "../../../../shared/container/injectionTokensMap";
+import { IStorageProvider } from "../../../../shared/infra/providers/storage/IStorageProvider";
 import { ICarsImageRepository } from "../../repositories/ICarsImageRepository";
 
 interface IInput{
@@ -9,12 +11,15 @@ interface IInput{
 @injectable()
 class UploadCarImageUseCase{
    constructor(
-      @inject("CarsImageRepository") private carsImageRepository: ICarsImageRepository
+      @inject("CarsImageRepository") private carsImageRepository: ICarsImageRepository,
+      @inject(injectionTokensMap.StorageProvider)
+      private storageProvider: IStorageProvider
    ){}
 
    async execute({car_id, images_name}: IInput) {
       images_name.forEach(async image_name => {
          await this.carsImageRepository.create(car_id, image_name)
+         await this.storageProvider.save(image_name, "cars")
       })
    }
 }
