91b60d793d0990c56fa4514535b2ea9d0e9eaef5
LuisFAOS <luisfaos.web@gmail.com>
2022-02-05 20:18:39 -0300

    adding toDTO user map and exposing avatar_url

--- src/modules/accounts/infra/typeorm/entities/User.ts
+++ src/modules/accounts/infra/typeorm/entities/User.ts
@@ -1,4 +1,5 @@
 import { v4 as uuidv4 } from 'uuid'
+import { Expose } from "class-transformer"
 
 import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm"
 
@@ -30,6 +31,22 @@ class User {
 
    @CreateDateColumn()
    created_at: Date;
+
+   @Expose({ name: "avatar_url" })
+   avatar_url(): string {
+      switch(process.env.disk){
+         case "s3":
+         return `${process.env.AWS_BUCKET_URL}/avatar/${this.avatar}`;
+
+         case "local":
+
+         return `${process.env.API_URL}/avatar/${this.avatar}`;
+
+         default:
+            console.log("invalid disk")
+         return;
+      }
+   }
 
    constructor(){
       if(!this.id){
