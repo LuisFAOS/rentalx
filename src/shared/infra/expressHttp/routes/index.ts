931581042105272364bc58c9a284dcd08c4d028c
LuisFAOS <luisfaos.web@gmail.com>
2022-02-05 20:11:59 -0300

    forgot password local email template

--- src/shared/infra/expressHttp/routes/index.ts
+++ src/shared/infra/expressHttp/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import { authenticatesRoutes } from './authenticates.routes'
 import { carsRoutes } from './cars.routes'
 import { categoriesRoutes } from './categories.routes'
+import { passwordRoutes } from './password.routes'
 import { rentalsRoutes } from './rental.routes'
 import { specificationsRoutes } from './specifications.routes'
 import { usersRoutes } from './users.routes'
@@ -13,6 +14,7 @@ router.use("/cars", carsRoutes)
 router.use("/categories", categoriesRoutes)          
 router.use("/specifications", specificationsRoutes)
 router.use("/rentals", rentalsRoutes)
+router.use("/password", passwordRoutes)
 router.use(authenticatesRoutes)
 
 export { router }
